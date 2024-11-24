859d590260716880756b47200f92247f224b85ef
Michael Still <mikal@stillhq.com>
2024-11-24 19:48:46 +1100

    Generate an all.txt file, add an apkg version.

--- validate.py
+++ validate.py
@@ -12,12 +12,17 @@ def _emit_error(ent, error, line):
 
 
 if __name__ == '__main__':
+    all = []
+
     for ent in os.listdir('.'):
         if not ent.endswith('.txt'):
             continue
 
         warnings = 0
         with open(ent) as f:
+            if ent == 'all.txt':
+                continue
+
             line = f.readline().rstrip()
             if line != '#separator:semicolon':
                 _emit_error(ent, 'first line is not "#separator:semicolon"', line)
@@ -37,17 +42,27 @@ if __name__ == '__main__':
                     _emit_error(ent, 'missing field separator', line)
                     warnings += 1
 
-                if line.find('; ') != -1:
-                    _emit_error(ent, 'whitespace after field separator', line)
-                    warnings += 1
-
-                if len(line.split(';')) > 2:
+                elif len(line.split(';')) > 2:
                     _emit_error(
                         ent,
                         'too many field separators, do not use semicolons',
                         line)
                     warnings += 1
 
+                elif line.find('; ') != -1:
+                    _emit_error(ent, 'whitespace after field separator', line)
+                    warnings += 1
+
+                else:
+                    all.append(line)
+
         if warnings > 0:
             print(f'>>> {warnings} warnings for file')
             print()
+
+        else:
+            with open('all.txt', 'w') as f:
+                f.write('#separator:semicolon\n')
+                f.write('#html:false\n')
+                for line in all:
+                    f.write(f'{line}\n')
